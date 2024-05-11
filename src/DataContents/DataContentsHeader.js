b9b3c96344f8d3724d683b7e2b4e4d96e5ed6b21
Apichat Wongtala <apichat26870@gmail.com>
2024-05-11 10:41:17 +0700

    add: file DataContents to commit

---
+++ src/DataContents/DataContentsHeader.js
@@ -0,0 +1,10 @@
+import resume from "../assets/Resume.pdf";
+
+export const Data_Header = {
+  name: "Apichat Wongtala",
+  title: "Junoi Front-end Developer · between jobs",
+  caption:
+    "From concept to reality, bridging the gap with code, transforming ideas into tangible digital solutions.",
+  btnview: "View Resume",
+  fileresume: resume,
+};
--- src/Sections/LeftSection/Header/Header.jsx
+++ src/Sections/LeftSection/Header/Header.jsx
@@ -1,28 +1,25 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowDown } from "@fortawesome/free-solid-svg-icons";
-import fileresume from "../../../assets/Resume.pdf";
+import { Data_Header } from "../../../DataContents/DataContentsHeader";
 
 const Header = () => {
   return (
     <div className="flex flex-col gap-2">
       <div className="text-3xl text-primaryTitle font-semibold">
-        Apichat Wongtala
+        {Data_Header.name}
       </div>
 
-      <div className="font-semibold">
-        Junoi Front-end Developer *between jobs
-      </div>
+      <div className="font-semibold">{Data_Header.title}</div>
 
       <div className="text-sm w-5/6 text-primaryContent">
-        From concept to reality, bridging the gap with code, transforming ideas
-        into tangible digital solutions.
+        {Data_Header.caption}
       </div>
 
       <div className="mt-4">
-        <a href={fileresume} target="_blank">
+        <a href={Data_Header.fileresume} target="_blank">
           <span className="bg-primaryTitle py-2 px-4 rounded-md">
-            View Resume
+            {Data_Header.btnview}
             <span className="inline-block rotate-90 ml-2 text-sm">
               <FontAwesomeIcon icon={faArrowDown} className="animate-bounce" />
             </span>
